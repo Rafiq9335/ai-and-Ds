8721b88dfe146508f249e778e6b9e9a40662fd06
Rafiq9335 <rafiq.2009khi@gmail.com>
2025-08-08 10:34:31 -0700

    uploading assignment foalders

---
+++ asignment'py.py
@@ -0,0 +1,64 @@
+#part1 arithmetic opretions
+a=15
+b=4
+
+print("Subtraction: a - b =", a - b)
+
+print("Multiplication: a * b =", a * b)
+
+print("Division: a / b =", a / b)
+
+print("Floor Division: a // b =", a // b)
+
+print("Modulus: a % b =", a % b)
+
+print("Exponentiation: a ** b =", a ** b)
+
+#part2arithmetic assignment oppretors
+x= 10
+print('value of x',x)
+x+=5
+print(x,"add 5")
+x*=2
+print(x,'mul_2')
+x-=4
+print(x,'sub4')
+x/=2
+print(x,'div_by_2=13')
+
+#part3 conparision opretors
+
+a = 7
+b = 10
+
+print("False",a == b)
+print("True", a != b)
+print("False", a > b)
+print("True", a < b)
+print("False", a >= b)
+print("True", a <= b)
+ 
+#part4 logical operatorsx = True
+x=True
+y = False
+
+print("False", x and y)
+print("True", x or y)
+print("True", not x)
+
+#part5 membership operators
+
+institute = "Saylani Mass IT"
+
+print("s in institute:", "s" in institute)
+print("Saylani in institute:", "Mass" in institute)
+print("Saylani not in institute:", "Saylani" not in institute)
+
+#part6 identity operators
+a=5
+b=5
+c=1000
+print("true",a is b)
+print("false",a is c)
+print("true",c is not b )
+
